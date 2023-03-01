22cfabb57f0714ba0008856c87979c598052989d
unknown <konancurry@gmail.com>
2023-03-01 18:55:05 +0000

    adding checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import photo from './star.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="Root">
+
+    
+    <div  className="Tree">
+      <div className="Branch">
+         <div className="Img">
+      <img src={photo} alt="logo" className="Log"></img>
+      </div> 
+     <div className="Box">
+     <div className="How">  <h1>How did we do ? </h1> </div>
+     <div className="Please"><h3>Please let us know how we did with your suppport request. All feedback is appreciated to help us improve our offering! </h3></div> 
+     </div>
+     <div className="Number">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+     </div>
+     <div className="Before">
+      <div className="End">
+        <h3>S</h3>
+        <h3>U</h3>
+        <h3>B</h3>
+        <h3>M</h3>
+        <h3>I</h3>
+        <h3>T</h3>
+
+        </div>
+        </div>
+
+      </div>
+   
+</div>
+
     </div>
+   
   );
 }
+
+
 
 export default App;
